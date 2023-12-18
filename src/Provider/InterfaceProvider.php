aa0a7084b068aa917402d954515a451b4d9c3131
fiction13 <fiction13@yandex.ru>
2023-12-18 15:16:47 +0300

    v1.0.1

--- src/Provider/InterfaceProvider.php
+++ src/Provider/InterfaceProvider.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * @package   plg_radicalmart_media_resize
- * @version   __DEPLOY_VERSION__
+ * @version   1.0.1
  * @author    Dmitriy Vasyukov - https://fictionlabs.ru
  * @copyright Copyright (c) 2023 Fictionlabs. All rights reserved.
  * @license   GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
